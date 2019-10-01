6501d14ad4e63939d71a5d5e77bd93b9d68ee3b9
root <chinenyediei@gmail.com>
2019-10-01 14:10:35 -0500

    added heroku setup

--- server.js
+++ server.js
@@ -32,7 +32,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+//connnect to Mongo DB
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
